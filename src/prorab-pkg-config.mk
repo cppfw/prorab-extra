300e652074dcd450786ff3a85ed945b80b2086cb
Ivan Gagis <igagis@gmail.com>
2020-01-25 13:14:55 +0200

    msys2 build fix

--- src/prorab-pkg-config.mk
+++ src/prorab-pkg-config.mk
@@ -1,24 +1,21 @@
 include prorab.mk
 
-#once
+# include guard
 ifneq ($(prorab_pkg_config_included),true)
     prorab_pkg_config_included := true
 
-
     define prorab-pkg-config
-        #need empty line here to avoid merging with adjacent macro instantiations
+        # need empty line here to avoid merging with adjacent macro instantiations
 
         install:: $(shell ls $(d)*.pc.in)
-		$(prorab_echo)myci-apply-version.sh -v `myci-deb-version.sh $(d)../debian/changelog` $(d)*.pc.in
-		$(prorab_echo)install -d $(DESTDIR)$(PREFIX)/lib/pkgconfig
-		$(prorab_echo)install -m 644 $(d)*.pc $(DESTDIR)$(PREFIX)/lib/pkgconfig
+$(.RECIPEPREFIX)$(a)myci-apply-version.sh -v `myci-deb-version.sh $(d)../debian/changelog` $(d)*.pc.in
+$(.RECIPEPREFIX)$(a)install -d $(DESTDIR)$(PREFIX)/lib/pkgconfig
+$(.RECIPEPREFIX)$(a)install -m 644 $(d)*.pc $(DESTDIR)$(PREFIX)/lib/pkgconfig
 
         uninstall::
-		$(prorab_echo)for f in $(notdir $(patsubst %.in,%,$(shell ls $(d)*.pc.in))); do \
-		    rm $(DESTDIR)$(PREFIX)/lib/pkgconfig/$$$$f; \
-		done
+$(.RECIPEPREFIX)$(a)for f in $(notdir $(patsubst %.in,%,$(shell ls $(d)*.pc.in))); do rm $(DESTDIR)$(PREFIX)/lib/pkgconfig/$$$$f; done
 
-        #need empty line here to avoid merging with adjacent macro instantiations
+        # need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 endif
