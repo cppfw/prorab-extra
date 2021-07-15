0e14657f608a24acb326c19bb8ed341519ae6b91
Ivan Gagis <igagis@gmail.com>
2021-07-15 17:13:32 +0300

    allow setting out dir fr doxygen and pkg config

--- src/prorab-doxygen.mk
+++ src/prorab-doxygen.mk
@@ -8,32 +8,35 @@ ifneq ($(prorab_doxygen_included),true)
 
     # doxygen docs are only possible for libraries, so install path is lib*-doc
     define prorab-build-doxygen
-        # need empty line here to avoid merging with adjacent macro instantiations
+
+        $(if $(this_name),,$(error prorab-build-doxygen: this_name is not defined))
 
         all: doc
 
-        doc:: $(d)doxygen
+        # NOTE: prorab_private_out_dir is defined by prorab.mk
 
-        $(d)doxygen.cfg: $(d)doxygen.cfg.in $(d)../debian/changelog
-$(.RECIPEPREFIX)$(a)myci-apply-version.sh -v $$(shell myci-deb-version.sh $(d)../debian/changelog) $$(firstword $$^)
+        doc:: $(d)$(prorab_private_out_dir)doxygen
 
-        $(d)doxygen: $(d)doxygen.cfg
-$(.RECIPEPREFIX)@echo "Building docs"
-$(.RECIPEPREFIX)$(a)(cd $(d); doxygen doxygen.cfg || true)
+        $(d)$(prorab_private_out_dir)doxygen.cfg: $(d)doxygen.cfg.in $(d)../debian/changelog
+$(if $(prorab_private_out_dir),$(.RECIPEPREFIX)$(a)mkdir -p $(d)$(prorab_private_out_dir))
+$(.RECIPEPREFIX)$(a)myci-apply-version.sh --version $$(shell myci-deb-version.sh $(d)../debian/changelog) $$(firstword $$^) --out-dir $(d)$(prorab_private_out_dir)
+
+        $(d)$(prorab_private_out_dir)doxygen: $(d)$(prorab_private_out_dir)doxygen.cfg
+$(.RECIPEPREFIX)@echo "build docs"
+$(.RECIPEPREFIX)$(a)(cd $(d)$(prorab_private_out_dir); doxygen doxygen.cfg || true) # ignore error, not all systems have doxygen
 
         clean::
-$(.RECIPEPREFIX)$(a)rm -rf $(d)doxygen
-$(.RECIPEPREFIX)$(a)rm -rf $(d)doxygen.cfg
-$(.RECIPEPREFIX)$(a)rm -rf $(d)doxygen_sqlite3.db
+$(.RECIPEPREFIX)$(a)rm -rf $(d)$(prorab_private_out_dir)doxygen
+$(.RECIPEPREFIX)$(a)rm -rf $(d)$(prorab_private_out_dir)doxygen.cfg
+$(.RECIPEPREFIX)$(a)rm -rf $(d)$(prorab_private_out_dir)doxygen_sqlite3.db
 
-        install:: $(d)doxygen
+        install:: $(d)$(prorab_private_out_dir)doxygen
 $(.RECIPEPREFIX)$(a)install -d $(DESTDIR)$(PREFIX)/share/doc/lib$(this_name)-doc
-$(.RECIPEPREFIX)$(a)install -m 644 $(d)doxygen/* $(DESTDIR)$(PREFIX)/share/doc/lib$(this_name)-doc || true #ignore error, not all systems have doxygen
+$(.RECIPEPREFIX)$(a)install -m 644 $(d)$(prorab_private_out_dir)doxygen/* $(DESTDIR)$(PREFIX)/share/doc/lib$(this_name)-doc || true # ignore error, not all systems have doxygen
 
         uninstall::
 $(.RECIPEPREFIX)$(a)rm -rf $(DESTDIR)$(PREFIX)/share/doc/lib$(this_name)-doc
 
-        # need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 endif
--- src/prorab-pkg-config.mk
+++ src/prorab-pkg-config.mk
@@ -5,17 +5,18 @@ ifneq ($(prorab_pkg_config_included),true)
     prorab_pkg_config_included := true
 
     define prorab-pkg-config
-        # need empty line here to avoid merging with adjacent macro instantiations
+
+        # NOTE: prorab_private_out_dir is defined by prorab.mk
 
         install:: $(shell ls $(d)*.pc.in)
-$(.RECIPEPREFIX)$(a)myci-apply-version.sh -v `myci-deb-version.sh $(d)../debian/changelog` $(d)*.pc.in
+$(if $(prorab_private_out_dir),$(.RECIPEPREFIX)$(a)mkdir -p $(d)$(prorab_private_out_dir))
+$(.RECIPEPREFIX)$(a)myci-apply-version.sh -v `myci-deb-version.sh $(d)../debian/changelog` $(d)*.pc.in --out-dir $(d)$(prorab_private_out_dir)
 $(.RECIPEPREFIX)$(a)install -d $(DESTDIR)$(PREFIX)/lib/pkgconfig
-$(.RECIPEPREFIX)$(a)install -m 644 $(d)*.pc $(DESTDIR)$(PREFIX)/lib/pkgconfig
+$(.RECIPEPREFIX)$(a)install -m 644 $(d)$(prorab_private_out_dir)*.pc $(DESTDIR)$(PREFIX)/lib/pkgconfig
 
         uninstall::
 $(.RECIPEPREFIX)$(a)for f in $(notdir $(patsubst %.in,%,$(shell ls $(d)*.pc.in))); do rm $(DESTDIR)$(PREFIX)/lib/pkgconfig/$$$$f; done
 
-        # need empty line here to avoid merging with adjacent macro instantiations
     endef
 
 endif
