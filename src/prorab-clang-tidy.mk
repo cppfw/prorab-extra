03f517fcc376acc77b5fa8a0708d4a4a5256ef8d
Ivan Gagis <igagis@gmail.com>
2022-04-16 19:35:55 +0300

    stuff

--- src/prorab-clang-tidy.mk
+++ src/prorab-clang-tidy.mk
@@ -6,13 +6,10 @@ ifneq ($(prorab_clang_tidy_included),true)
 
 define prorab-private-clang-tidy-single-source-file
 
-    $(eval prorab_private_this_clang_tidy_flags := --header-filter=.* --warnings-as-errors=*)
-    $(eval prorab_private_this_clang_tidy_flags += $(if $(this_clang_tidy_checks),--checks="$(subst $(prorab_space),$(prorab_comma),$(this_clang_tidy_checks))",))
-
     .PHONY:
     clang-tidy__$1:
 $(.RECIPEPREFIX)@echo "clang-tidy $1"
-$(.RECIPEPREFIX)$(a)(cd $(d) && clang-tidy $1 $(prorab_private_this_clang_tidy_flags) -- $(this_cppflags) $(this_cxxflags))
+$(.RECIPEPREFIX)$(a)(cd $(d) && clang-tidy $1 -- $(this_cppflags) $(this_cxxflags))
 
 endef
 
