5e7ff028918e776de6dca7fbc0ae294144eae03a
Ivan Gagis <igagis@gmail.com>
2022-05-14 15:41:31 +0300

    add prorab-clang-format

---
+++ src/prorab-clang-format.mk
@@ -0,0 +1,33 @@
+include prorab.mk
+
+# include guard
+ifneq ($(prorab_clang_format_included),true)
+    prorab_clang_format_included := true
+
+# macosx at the moment doesn't provide clang-format version 15, so just
+# don't perform clang-format cheks on macosx.
+# TODO: remove the ifeq when macosx homebrew adds a formulae for clang-format 15
+ifeq ($(os),macosx)
+    prorab-clang-format :=
+else
+    define prorab-clang-format
+
+        $(eval prorab_private_src_suffixes := $(if $(this_src_suffixes),$(this_src_suffixes),c h cpp hpp cxx hxx))
+        $(eval prorab_private_src_dir := $(if $(this_src_dir),$(this_src_dir),.))
+
+        $(eval prorab_private_format_find_patterns := -name *.$(firstword $(prorab_private_src_suffixes)) \
+                $(foreach s,$(wordlist 2,$(words $(prorab_private_src_suffixes)),$(prorab_private_src_suffixes)),-or -name *.$(s)))
+        $(eval prorab_private_format_srcs := $(shell cd $(d) && find $(prorab_private_src_dir) -type f $(prorab_private_format_find_patterns)))
+
+        test::
+$(.RECIPEPREFIX)@echo "check format"
+$(.RECIPEPREFIX)$(a)(cd $(d) && clang-format --dry-run --Werror $(prorab_private_format_srcs))
+
+        apply-format:
+$(.RECIPEPREFIX)@echo "apply format"
+$(.RECIPEPREFIX)$(a)(cd $(d) && clang-format -i --Werror $(prorab_private_format_srcs))
+
+    endef
+endif
+
+endif
