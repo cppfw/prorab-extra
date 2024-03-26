d06537324289643ef4e3be16fcaa31aa10a2d5ad
Ivan Gagis <igagis@gmail.com>
2024-03-26 15:17:56 +0200

    detect archlinux

--- src/prorab-linux.mk
+++ src/prorab-linux.mk
@@ -5,8 +5,17 @@ ifneq ($(prorab_linux_included),true)
     prorab_linux_included := true
 
 	ifeq ($(os),linux)
-    	prorab_linux := $(shell lsb_release --id --short | tr '[:upper:]' '[:lower:]')
-		prorab_linux_codename := $(shell lsb_release --codename --short)
+    	prorab_linux := $(shell (lsb_release --short --id 2>/dev/null || true) | tr '[:upper:]' '[:lower:]')
+		prorab_linux_codename := $(shell lsb_release --codename --short 2>/dev/null || true)
+
+		ifeq ($(prorab_linux),)
+			prorab_linux := $(if $(shell pacman --version 2>/dev/null || true),"archlinux")
+			prorab_linux_codename :=
+		endif
+
+		ifeq ($(prorab_linux),)
+			prorab_linux_codename :=
+		endif
 	endif
 
 endif # ~include guard
