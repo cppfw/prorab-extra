f4e2726c3ec5ad9e6a4298388ddc9af1eb38e377
Ivan Gagis <igagis@gmail.com>
2024-12-02 16:12:22 +0200

    conan: minor renaming

--- conan/test_package/conanfile.py
+++ conan/test_package/conanfile.py
@@ -9,4 +9,4 @@ class TestConan(ConanFile):
 		self.requires(self.tested_reference_str)
 
 	def test(self):
-		self.run("make $MAKE_INCLUDE_DIR_ARGS", env="conanrun") # env sets LD_LIBRARY_PATH etc. to find dependency libs
+		self.run("make $MAKE_INCLUDE_DIRS_ARG", env="conanrun") # env sets LD_LIBRARY_PATH etc. to find dependency libs
